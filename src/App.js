2b3a6369a0c6b680ffd677bcce5fb9e80a4b9cbb
Megan O'Brien <116932368+megan-o@users.noreply.github.com>
2023-02-15 01:15:45 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <br/>
+        <img src="obrien-megan.jpg" className="App-logo" alt="logo" />
+        <h3>
+          Megan O'Brien
+        </h3>
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Senior Analyst - Primary Research
         </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='body'>
+          Welcome to the online presence of Megan O'Brien, a Senior Analyst in Primary Research at Omnicom Media Group. Megan is a highly motivated and passionate professional with a wealth of experience in the advertising and marketing industry. Her dedication to excellence and commitment to personal growth have been the driving forces behind her success, both as an individual contributor and as part of a larger team.
+        </p>
+        <p className='body'>
+        Megan's talent, hard work, and commitment to personal and professional growth make her a valuable asset to any team. Whether working as an individual contributor or collaborating with others, she brings a positive and enthusiastic attitude to everything she does.
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <br />
+          <br />
+          <br />
+          <br />
+          <div className="App-link">
+            <a href="https://www.facebook.com/megan.siobhan.1"><img src="https://img.icons8.com/fluent/48/000000/facebook-new.png" alt="Facebook" /></a>
+            <a href="https://twitter.com/beggingformegan"><img src="https://img.icons8.com/fluent/48/000000/twitter.png" alt="Twitter" /></a>
+            <a href="https://www.linkedin.com/in/meganscobrien/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" /></a>
+            <a href="mailto:meganscobrien@gmail.com?subject=Hello%20World&body=How%20are%20you?"><img src="https://img.icons8.com/fluent/48/000000/mail.png" alt="Email" /></a>
+          </div>
         </p>
+        <p className="small">2023 Megan</p>
       </header>
     </div>
   );
