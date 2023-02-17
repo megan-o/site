306c9c625160c1183bf212a0b5b1d1dd0c8c2db8
Megan O'Brien <116932368+megan-o@users.noreply.github.com>
2023-02-17 00:01:57 +0000

    removed justification

--- src/Components/MyCard.js
+++ src/Components/MyCard.js
@@ -3,13 +3,13 @@ import { ListGroupItem } from 'react-bootstrap';
 function MyCard(items) {
   return (
     <ListGroupItem style={{ borderBottom: '1px solid white' }}>
-      <div className="d-flex w-100">
+      <div className="d-flex w-100 justify-content-between">
       <small> <strong>{items.items.Date}</strong></small>
         <h5>
           <a href={items.items.Link} style={{ color: 'white' }} target="_blank">{items.items.Name}</a>
         </h5>
       </div>
-      <p>{items.items.Description}</p>
+      <p style={{textAlign: 'left'}}>{items.items.Description}</p>
     </ListGroupItem>
   );
 }
