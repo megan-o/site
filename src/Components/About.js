9d7c80e6fbff93d9645ce02371b1a51ad11e1066
Megan O'Brien <116932368+megan-o@users.noreply.github.com>
2025-04-01 21:54:29 -0400

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,18 +3,10 @@ function About() {
       <>
       <br/>
         <p className="body">
-          Welcome to the online presence of Megan O'Brien, a Senior Analyst in
-          Primary Research at Omnicom Media Group. Megan is a highly motivated and
-          passionate professional with a wealth of experience in the advertising
-          and marketing industry. Her dedication to excellence and commitment to
-          personal growth have been the driving forces behind her success, both as
-          an individual contributor and as part of a larger team.
+          Consumer research and insights professional with over 5 years of experience. Expert in conducting quantitative and qualitative research including, but not limited to, segmentation, brand health, customer journey, and concept testing. Extensive experience in research instrument design, survey programming, data analysis, and storytelling. Passionate about understanding consumer motivations/behaviors and enabling brands to better engage with their consumers in relevant and meaningful ways. Academic achievements include a master’s degree in psychology from the University of Edinburgh and a bachelor’s degree with a dual major in biology and psychology from Stony Brook University.
         </p>
         <p className="body">
-          Megan's talent, hard work, and commitment to personal and professional
-          growth make her a valuable asset to any team. Whether working as an
-          individual contributor or collaborating with others, she brings a
-          positive and enthusiastic attitude to everything she does.
+         Feel free to connect with me or contact me via email with any opportunities or queries.
         </p>
       </>
     );
